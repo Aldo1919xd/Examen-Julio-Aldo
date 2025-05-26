ef11efb8119d44008213cdd02a60b0740cfbe8e8
Aldo <calebsamanamudxd@gmail.com>
2025-05-26 14:48:17 -0500

    modelo dto

--- src/main/java/dto/Docente.java
+++ src/main/java/dto/Docente.java
@@ -4,10 +4,76 @@
  */
 package dto;
 
+import java.util.Date;
+
 /**
  *
  * @author caleb
  */
 public class Docente {
-    
+
+    private String Codido;
+    private String Nombre;
+    private String Direccion;
+    private Date FechaNacimiento;
+    private double Talla;
+    private String Correo;
+
+    public Docente(String Codido, String Nombre, String Direccion, Date FechaNacimiento, double Talla, String Correo) {
+        this.Codido = Codido;
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.FechaNacimiento = FechaNacimiento;
+        this.Talla = Talla;
+        this.Correo = Correo;
+    }
+
+    public String getCodido() {
+        return Codido;
+    }
+
+    public void setCodido(String Codido) {
+        this.Codido = Codido;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+    public double getTalla() {
+        return Talla;
+    }
+
+    public void setTalla(double Talla) {
+        this.Talla = Talla;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
 }
