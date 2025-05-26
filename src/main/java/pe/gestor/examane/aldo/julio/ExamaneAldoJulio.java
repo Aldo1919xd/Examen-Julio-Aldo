0ba9fbaaa4ed0dd9337e17eb501cc5ff4319b993
Aldo <calebsamanamudxd@gmail.com>
2025-05-26 12:55:38 -0500

    aaa

---
+++ src/main/java/pe/gestor/examane/aldo/julio/ExamaneAldoJulio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.gestor.examane.aldo.julio;
+
+/**
+ *
+ * @author caleb
+ */
+public class ExamaneAldoJulio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
